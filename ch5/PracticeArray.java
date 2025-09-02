da27e5db6fa7d0183dac6bff37d3dd8269b3f318
molcham <130418659+molcham@users.noreply.github.com>
2025-09-03 00:21:16 +0900

    2025/09/03 ch5/ArrayEx1.java

--- ch5/PracticeArray.java
+++ ch5/PracticeArray.java
@@ -7,18 +7,18 @@ public class PracticeArray {
 
         score[0]=50;
         score[1]=60;
-        score[k+1] = 70;
+        score[k+1] = 70; // 1.배열의 인덱스로 변수도 넣을 수 있음 ! 
         score[3] = 80;
         score[4] = 90;
 
-        int tmp =score[k+2] + score[4];
+        int tmp =score[k+2] + score[4]; //2.배열의 인덱스로 변수 사용
 
         for(int i=0; i < 5; i++){
             System.out.printf("score[%d]:%d%n",i,score[i]);
 
         }
         System.out.printf("tmp:%d%n",tmp);
-        System.out.printf("score[%d]:%d%n",7,score[7]);
+        System.out.printf("score[%d]:%d%n",7,score[7]); // 3.index의 범위를 벗어난 값
     }
     
 }
